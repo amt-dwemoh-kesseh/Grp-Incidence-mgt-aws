be0dd2ede13bd3fbca8d61f0a6c447c287c98910
Derrick Asamoah <asamoahderrick7@gmail.com>
2025-08-28 17:45:20 +0000

    add cors origin

--- src/incidents/listIncidents.js
+++ src/incidents/listIncidents.js
@@ -2,8 +2,8 @@ const AWS = require("aws-sdk");
 
 const CORS_HEADERS = {
   "Access-Control-Allow-Origin":
-    "http://localhost:4200,https://dev.d2zgxshg38rb8v.amplifyapp.com",
-  "Access-Control-Allow-Methods": "OPTIONS,POST",
+    "http://localhost:4200, https://dev.d2zgxshg38rb8v.amplifyapp.com",
+  "Access-Control-Allow-Methods": "OPTIONS, POST, GET, PUT",
   "Access-Control-Allow-Headers": "Content-Type,Authorization",
 };
 
@@ -11,20 +11,20 @@ exports.handler = async (event) => {
   try {
     // Configure AWS SDK with region and timeout
     AWS.config.update({
-      region: process.env.AWS_REGION || "us-east-1",
+      region: process.env.AWS_REGION || "eu-central-1",
       httpOptions: {
         timeout: 5000,
         connectTimeout: 5000,
       },
     });
 
-    if (event['httpMethod'] == 'OPTIONS') {
-        return {
-            "statusCode": 200,
-            "headers": CORS_HEADERS,
-            "body": ""
-        }
-      }
+    if (event["httpMethod"] == "OPTIONS") {
+      return {
+        statusCode: 200,
+        headers: CORS_HEADERS,
+        body: "",
+      };
+    }
 
     const dynamo = new AWS.DynamoDB.DocumentClient();
 
