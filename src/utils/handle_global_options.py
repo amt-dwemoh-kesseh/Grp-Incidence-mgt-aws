bdf7d66f5eba22b7800ca30df4abd931527a100b
Derrick Asamoah <asamoahderrick7@gmail.com>
2025-09-01 10:51:09 +0000

    refactor cors

--- src/utils/handle_global_options.py
+++ src/utils/handle_global_options.py
@@ -1,22 +1,24 @@
 import os
 
-
-AMPLIFY_DOMAIN = os.environ["AMPLIFY_DOMAIN"]
+AMPLIFY_PROD_DOMAIN = os.environ["AMPLIFY_PROD_DOMAIN"]
+AMPLIFY_DEV_DOMAIN = os.environ["AMPLIFY_DEV_DOMAIN"]
 
 ALLOWED_ORIGINS = [
-	"http://localhost:4200",
-	"https://your-production-domain.com",
-	AMPLIFY_DOMAIN
+	"http://localhost:4200/",
+	AMPLIFY_DEV_DOMAIN,
+	AMPLIFY_PROD_DOMAIN
 ] 
 
 def handler(event, context):
-	
-	origin = event['headers'].get('Origin')
-	if origin in ALLOWED_ORIGINS:
+
+	referer = event['headers'].get('Referer')
+	print(f"HEADERS: {event['headers']}")
+	print(f"Received OPTIONS request from referer: {referer}")
+	if referer in ALLOWED_ORIGINS:
 		return {
 			"statusCode": 200,
 			"headers": {
-				"Access-Control-Allow-Origin": origin,
+				"Access-Control-Allow-Origin": referer,
 				"Access-Control-Allow-Headers": "Content-Type,Authorization,X-Amz-Date",
 				"Access-Control-Allow-Methods": "OPTIONS,GET,POST,PUT"
 			}
